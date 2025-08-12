bf15c3f76f215ee031ea5c4c01e169028c7f8fb4
ahmed kasban <ahmedkasban520@gmail.com>
2025-08-12 07:33:55 +0300

    Add Person as an entity

---
+++ CourseManagementSystem/Entities/Person.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CourseManagementSystem.Entities
+{
+    public class Person
+    {
+
+        private string name;
+        private int age;
+        private readonly int Person_ID;
+        private char gender;
+        private string email;
+        private string phone;
+        private string nationalID;
+        public string Name
+        {
+            get => name;
+            set => name = value ?? "Known";
+
+        }
+        public int Age
+        {
+            get => age;
+            set => age = value < 18 ? 18 : value;
+        }
+        public int ID
+        {
+            get => Person_ID;
+        }
+        public char Gender
+        {
+            get => gender;
+            set
+            {
+                char V = char.ToUpper(value);
+                gender = V == 'M' || V == 'F' ? V : 'N';
+            }
+        }
+        public string Phone
+        {
+            get => phone;
+            set => phone = string.IsNullOrWhiteSpace(value) ? "000-0000000" : value;
+        }
+        public string Email
+        {
+            get => email;
+            set => email = string.IsNullOrWhiteSpace(value) ? "unknown@example.com" : value;
+        }
+        public string NationalID
+        {
+            get => nationalID;
+            private set => nationalID = string.IsNullOrWhiteSpace(value) ? "N/A" : value;
+        }
+
+        // When Create Person 
+        public Person(string name, int age, char gender, string email, string phone, string nationalID)
+        {
+            Name = name;
+            Age = age;
+            Gender = gender;
+            Email = email;
+            Phone = phone;
+            NationalID = nationalID;
+        }
+
+        // When Read Data from Database
+        public Person(int id, string name, int age, char gender, string email, string phone, string nationalID)
+            : this(name, age, gender, email, phone, nationalID)
+        {
+            this.Person_ID = id;
+        }
+    }
+}
