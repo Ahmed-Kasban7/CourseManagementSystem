4965e772324b594a5537567b2f33394a2e878528
ahmed kasban <ahmedkasban520@gmail.com>
2025-08-21 07:13:56 +0300

    Add New File InterFaces to contain interfaces

---
+++ CourseManagementSystem/Interfaces/IAccount.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CourseManagementSystem.Interfaces
+{
+    public interface IAccount
+    {
+       string UserName { get;}
+       string Password { get; set; }
+
+    }
+}
