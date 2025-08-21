4ea745b302a16efdc33fb6b28ff5023dae9c6b8a
ahmed kasban <ahmedkasban520@gmail.com>
2025-08-21 07:16:20 +0300

    edit some things in Student

--- CourseManagementSystem/Entities/Student.cs
+++ CourseManagementSystem/Entities/Student.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CourseManagementSystem.Interfaces;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
@@ -10,22 +11,27 @@ namespace CourseManagementSystem.Entities
     public class Student:Person ,IAccount
     {
         private readonly int studentID;
-        public string UserName { get;}
+        private int levelID;
+        public int LevelID {
+            get => levelID;
+            set => levelID = value;
+        }
+        public string UserName => nationalID;
         public string Password { get; set; }
         public int StudentID => studentID;
         // When Create Student
-        public Student(string name, int age, char gender, string email, string phone, string nationalID , string password)
+        public Student(string name, int age, char gender, string email, string phone, string nationalID,int levelID , string password)
             : base(name, age, gender, email, phone, nationalID)
         {
-            UserName = nationalID;
+            this.levelID= levelID;
             Password = password;
         }
 
         // When Reading student from database
-        public Student(int studentID,int PersonID,string name, int age, char gender, string email, string phone, string nationalID, string password )
+        public Student(int studentID,int PersonID,string name, int age, char gender, string email, string phone,  string nationalID, int levelID, string password )
             : base(PersonID, name, age, gender, email, phone, nationalID)
         {
-            this.UserName = nationalID;
+            this.levelID = levelID;
             this.Password = password;
             this.studentID = studentID;
         }
