cd6a265b3272802003797983703abc20129380ca
ahmed kasban <ahmedkasban520@gmail.com>
2025-08-21 07:18:45 +0300

    edit Level and Admin and Semester class

--- CourseManagementSystem/Entities/Level.cs
+++ CourseManagementSystem/Entities/Level.cs
@@ -6,7 +6,38 @@ using System.Threading.Tasks;
 
 namespace CourseManagementSystem.Entities
 {
-    internal class Level
+    public class Level
     {
+        private readonly int id;
+        private string name;
+        private string description;
+        public int ID => id;
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+        public string Description
+        {
+            get { return description; }
+            set { description = value; }
+        }
+
+        // When Create New Level
+        public Level(string name, string description)
+        {
+            this.name = name;
+            this.description = description;
+        }
+
+        // When retrive Levels Data from Data Base
+        public Level(int id, string name, string description)
+            : this(name, description)
+        {
+            this.id = id;
+        }
+
+
+
     }
 }
