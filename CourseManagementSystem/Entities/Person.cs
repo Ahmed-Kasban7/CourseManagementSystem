5ad141a130e34b1345c38ca53aa371fb0815fe54
ahmed kasban <ahmedkasban520@gmail.com>
2025-08-22 07:26:17 +0300

    edit Fields in Person class

--- CourseManagementSystem/Entities/Person.cs
+++ CourseManagementSystem/Entities/Person.cs
@@ -9,52 +9,14 @@ namespace CourseManagementSystem.Entities
     abstract public class Person
     {
 
-       protected string name;
-       protected int age;
-       protected readonly int personID;
-       protected char gender;
-       protected string email;
-       protected string phone;
-       protected string nationalID;
-        public string Name
-        {
-            get => name;
-            set => name = value ?? "Known";
+        public string Name { get; private set; }
+        public int Age { get; private set; }
+        public  int PersonID { get; private set; }
+        public char Gender { get; private set; }
+        public string Email { get; private set; }
+        public string Phone { get; private set; }
+        public string NationalID { get; private set; }
 
-        }
-        public int Age
-        {
-            get => age;
-            set => age = value < 18 ? 18 : value;
-        }
-        public int PersonID
-        {
-            get => personID;
-        }
-        public char Gender
-        {
-            get => gender;
-            set
-            {
-                char V = char.ToUpper(value);
-                gender = V == 'M' || V == 'F' ? V : 'N';
-            }
-        }
-        public string Phone
-        {
-            get => phone;
-            set => phone = string.IsNullOrWhiteSpace(value) ? "000-0000000" : value;
-        }
-        public string Email
-        {
-            get => email;
-            set => email = string.IsNullOrWhiteSpace(value) ? "unknown@example.com" : value;
-        }
-        public string NationalID
-        {
-            get => nationalID;
-            private set => nationalID = string.IsNullOrWhiteSpace(value) ? "N/A" : value;
-        }
 
         // When Create Person 
         public Person(string name, int age, char gender, string email, string phone, string nationalID)
@@ -71,7 +33,7 @@ namespace CourseManagementSystem.Entities
         public Person(int id, string name, int age, char gender, string email, string phone, string nationalID)
             : this(name, age, gender, email, phone, nationalID)
         {
-            this.personID = id;
+            this.PersonID = id;
         }
     }
 }
