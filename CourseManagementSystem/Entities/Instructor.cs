76f729331d604566b54a81d279444aaa10986f05
ahmed kasban <ahmedkasban520@gmail.com>
2025-08-22 07:24:55 +0300

    Add some Fields in Instuctor class

--- CourseManagementSystem/Entities/Instructor.cs
+++ CourseManagementSystem/Entities/Instructor.cs
@@ -7,35 +7,45 @@ using System.Threading.Tasks;
 
 namespace CourseManagementSystem.Entities
 {
+    public enum EnTitle
+    {
+        Professor ,
+        TA
+    }
     public class Instructor:Person , IAccount
     {
-        private readonly int instructorID;
-        private readonly int? departmentID;
-        public string UserName => nationalID;
+        public  int InstructorID { get; private set; }
+        public int? DepartmentID { get; private set; }
+        public DateTime HireDate { get; private set; }
+        public EnTitle Title { get; private set; }
+        public string Specialization { get; private set; }
+        public string UserName => base.NationalID;
         public string Password { get; set; }
-        public int InstructorID => instructorID;
-        public int? DepartmentID => departmentID;
-
-
+        
 
         // When Create instructor 
-        public Instructor(string name, int age, char gender, string email, string phone, string nationalID , string password, int? departmentID = null)
+        public Instructor(string name, int age, char gender, string email, string phone, string nationalID , string password, EnTitle title, string specialization, int? departmentID = null)
             : base(name, age, gender, email, phone, nationalID)
         {
             this.Password = password;
-            this.departmentID = departmentID;
+            this.DepartmentID = departmentID;
+            Title = title;
+            Specialization = specialization;
+            HireDate = DateTime.Now;
         }
 
-        
-        // When Reading From Database  
-        public Instructor(int instructorID, int departmentID, int PersonID, string name, int age, char gender, string email, string phone
-            , string nationalID ,string password)
-            : base(PersonID, name, age, gender, email, phone, nationalID)
-        {
-            this.Password = password;
-            this.instructorID = instructorID;
-            this.departmentID = departmentID;
 
+        // When Reading From Database  
+        public Instructor(int instructorID, int departmentID, int personID, string name, int age, char gender,
+                             string email, string phone, string nationalID, string password, EnTitle title, string specialization)
+               : base(personID, name, age, gender, email, phone, nationalID)
+        {
+            InstructorID = instructorID;
+            DepartmentID = departmentID;
+            Password = password;
+            Title = title;
+            Specialization = specialization;
+            HireDate = DateTime.Now; 
         }
 
     }
