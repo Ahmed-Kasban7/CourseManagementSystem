15ae851e46467e6b7a09bbd5616aec74e2529d8e
ahmed kasban <ahmedkasban520@gmail.com>
2025-08-23 09:32:13 +0300

    Add Semester class

--- CourseManagementSystem/Entities/Semester.cs
+++ CourseManagementSystem/Entities/Semester.cs
@@ -6,8 +6,27 @@ using System.Threading.Tasks;
 
 namespace CourseManagementSystem.Entities
 {
+    public enum enSemesterType {
+        Fall,
+        Spring ,
+        Summer
+
+    }
+    public enum enSemesterStatus {
+        Upcoming,
+        RegistrationOpen ,
+        Running ,
+        Completed ,
+        Cancelled
+    }
+
     public class Semester
     {
+
+        public int SemesterId { get; set; }   
+        public enSemesterType SemesterType { get; set; }      
+        public int StartMonth { get; set; }   
+        public int DurationInMonths { get; set; }
 
     }
 }
