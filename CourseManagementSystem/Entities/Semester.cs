cf7e32ab39f12f552e6ee5d1d2e4232fee1d8550
ahmed kasban <ahmedkasban520@gmail.com>
2025-08-24 10:03:09 +0300

    Modified Semester class and add stMonth and day

--- CourseManagementSystem/Entities/Semester.cs
+++ CourseManagementSystem/Entities/Semester.cs
@@ -12,21 +12,42 @@ namespace CourseManagementSystem.Entities
         Summer
 
     }
-    public enum enSemesterStatus {
-        Upcoming,
-        RegistrationOpen ,
-        Running ,
-        Completed ,
-        Cancelled
-    }
+ 
 
     public class Semester
     {
+        public readonly struct StMonthDay {
+            public int Month { get; }
+            public int Day { get; }
+            public StMonthDay(int month, int day)
+            {
+                Month = month;
+                Day = day;
+            }
+        }
 
-        public int SemesterId { get; set; }   
-        public enSemesterType SemesterType { get; set; }      
-        public int StartMonth { get; set; }   
-        public int DurationInMonths { get; set; }
 
+        public int SemesterID { get; private set; }   
+        public enSemesterType SemesterType { get; private set; }      
+        public StMonthDay StartMonth { get; private set; }   
+        public int DurationInMonths { get; private set; }
+
+        public int RegistrationOpenBeforeStart { get; private set; }
+        // When Create New Semester
+        public Semester(enSemesterType semesterType, StMonthDay startMonth, int durationInMonths, int registrationOpenBeforeStart)
+        {
+            SemesterType = semesterType;
+            StartMonth = startMonth;
+            DurationInMonths = durationInMonths;
+            RegistrationOpenBeforeStart = registrationOpenBeforeStart;
+        }
+
+        // When Retrive data from DB
+
+        public Semester(int semesterID,enSemesterType semesterType, StMonthDay startMonth, int durationInMonths , int registrationOpenBeforeStart)
+         : this (semesterType , startMonth , durationInMonths , registrationOpenBeforeStart)
+        {
+            this.SemesterID = semesterID;
+        }
     }
 }
